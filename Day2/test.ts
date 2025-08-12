1f677e37bd623b24b83539f2f6f2d7ae9c0e6232
Shakib-ewu <hossainsakib58@gmail.com>
2025-08-12 13:33:09 +0600

    Day2

---
+++ Day2/test.ts
@@ -0,0 +1,19 @@
+//Strings are written inside double or single quotes. Numbers are written without quotes.
+let newMessage = "Hello"
+let count = 30
+
+newMessage = "100";
+console.log(newMessage);
+
+let x = 5;
+x = 7;
+x = x + x;
+alert(x); // This will show 14
+
+const car: { type: string; model: string; color: string; owner?: string } = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
