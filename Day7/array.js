c1ce94bb978e54b728614b0796096ad173f2bcab
Shakib-ewu <hossainsakib58@gmail.com>
2025-08-21 16:46:05 +0600

    Day7

---
+++ Day7/array.js
@@ -0,0 +1,15 @@
+const sampleArray = [1, 2, 3, 4, 5];
+
+const stringArray = ["apple", "banana", "cherry", "date","elderberry"];
+console.log(stringArray);
+console.log(stringArray.toString());
+console.log(stringArray[3]);
+console.log(stringArray.length);
+
+//last item of array
+console.log(stringArray[stringArray.length - 1]);
+
+// Get last index of the array
+console.log("Last index:", stringArray.length - 1);
+console.log(stringArray.join("* "));
+
